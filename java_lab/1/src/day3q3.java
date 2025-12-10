064b443832cb7bfc17f605ab1ef6ba5f899a0b92
arnabcse-gif <arnabchakraborty922@gmail.com>
2025-12-10 13:15:41 +0530

    Create day3q3.java

---
+++ java_lab/1/src/day3q3.java
@@ -0,0 +1,19 @@
+public class day3q3 {
+}
+ class WeekDay {
+    public static void main(String[] args) {
+
+        int dayNo = 3;   // Mention the day number here
+
+        switch (dayNo) {
+            case 1: System.out.println("Sunday"); break;
+            case 2: System.out.println("Monday"); break;
+            case 3: System.out.println("Tuesday"); break;
+            case 4: System.out.println("Wednesday"); break;
+            case 5: System.out.println("Thursday"); break;
+            case 6: System.out.println("Friday"); break;
+            case 7: System.out.println("Saturday"); break;
+            default: System.out.println("Invalid Day Number");
+        }
+    }
+}
