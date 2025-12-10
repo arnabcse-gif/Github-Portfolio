53c50c407283a82665c6dd62808d003cf3beb558
arnabcse-gif <arnabchakraborty922@gmail.com>
2025-12-10 23:28:58 +0530

    Create day2q1.java

---
+++ java_lab/1/src/day2q1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class EvenOddCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int even = 0, odd = 0;
+
+        System.out.println("Enter 10 numbers:");
+
+        for (int i = 1; i <= 10; i++) {
+            int num = sc.nextInt();
+
+            if (num % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        System.out.println("Number of Even = " + even);
+        System.out.println("Number of Odd = " + odd);
+    }
+}
